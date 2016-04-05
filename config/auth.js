fb16c7ca11ceb8c0cd01bfd5a099ba85c984d98f
Shaun Fowler <shaun.fowler@gmail.com>
2016-04-04 21:28:22 -0400

    delete auth.js

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    allowedEmailAddresses: [
-        'john.smith@example.com'
-    ],
-    google: {
-        clientID: 'client-id-goes-here',
-        clientSecret: 'client-secret-goes-here'
-    }
-};
